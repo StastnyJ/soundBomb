e42c1e195f1f5ca5e3e3b55b919204a5fbe3b4d0
StastnyJ <xstastnyj@gmail.com>
2023-02-08 20:01:39 +0100

    ~ song detection

--- src/main.ino
+++ src/main.ino
@@ -1,9 +1,27 @@
 #include "./tones.h"
+#include "SSD1306Wire.h"
 #include "arduinoFFT.h"
 
 #define MICROPHONE_INPUT_PIN 15
+#define LED_PIN 18
+#define I2C_SDA 21
+#define I2C_SCL 22
+
 #define SAMPLES 512
 #define SAMPLING_FREQUENCY 40000 // Hz
+#define TONES_BATCH_SIZE 5
+#define TONES_BATCH_MINIMUM 4
+#define TONES_BATCH_TOLERANCE 0
+#define SONG_TOLERANCE 1
+#define SONG_TONE_WAIT 2200 // ms
+#define LED_ON_TIME 200     // ms
+#define LED_OFF_TIME 800    // ms
+
+#define SONG_LENGTH 19
+int song[SONG_LENGTH] = {C5, E5, G5, C5, E5, G5, E5, D5, E5, F5, D5, E5, D5, E5, F5, D5, E5, D5, C5};
+int songProgress = 0;
+unsigned int relativeProgress = 0;
+unsigned long nextToneDeadline = 0;
 
 unsigned int sampling_period_us;
 unsigned long newTime, oldTime;
@@ -12,31 +30,96 @@ double vReal[SAMPLES];
 double vImg[SAMPLES];
 
 char toneString[4] = {0, 0, 0, 0};
+int tonesBatch[TONES_BATCH_SIZE];
+
+int ledStatus = 0;
+int nextLedChange = 0;
 
 arduinoFFT FFT = arduinoFFT();
+SSD1306Wire display(0x3c, I2C_SDA, I2C_SCL);
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Board init complete");
   sampling_period_us = round(1000000 * (1.0 / SAMPLING_FREQUENCY));
+  pinMode(LED_PIN, OUTPUT);
+  display.init();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_16);
 }
 
 void loop() {
-  for (int i = 0; i < SAMPLES; i++) {
-    newTime = micros() - oldTime;
-    oldTime = newTime;
-    vReal[i] = analogRead(MICROPHONE_INPUT_PIN);
-    vImg[i] = 0;
-    while (micros() < (newTime + sampling_period_us)) {
+  if (songProgress >= SONG_LENGTH) {
+    display.clear();
+    display.display();
+    digitalWrite(LED_PIN, LOW);
+  } else {
+    digitalWrite(LED_PIN, ledStatus);
+    for (int actToneIdx = 0; actToneIdx < TONES_BATCH_SIZE; actToneIdx++) {
+      for (int i = 0; i < SAMPLES; i++) {
+        newTime = micros() - oldTime;
+        oldTime = newTime;
+        vReal[i] = analogRead(MICROPHONE_INPUT_PIN);
+        vImg[i] = 0;
+        while (micros() < (newTime + sampling_period_us)) {
+        }
+      }
+      FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+      FFT.Compute(vReal, vImg, SAMPLES, FFT_FORWARD);
+      FFT.ComplexToMagnitude(vReal, vImg, SAMPLES);
+      double freq = FFT.MajorPeak(vReal, SAMPLES, SAMPLING_FREQUENCY) / 2.0;
+
+      tonesBatch[actToneIdx] = freqToTone(freq);
+    }
+    int tone = selectToneFromBatch();
+    if (tone >= 0) {
+      toneToString(tone, toneString);
+      if (nextToneDeadline < millis()) {
+        songProgress = 0;
+      }
+      if (abs(tone - song[songProgress]) <= SONG_TOLERANCE) {
+        songProgress++;
+        relativeProgress = (uint)(100.0f * ((float)songProgress) / (SONG_LENGTH));
+        nextToneDeadline = millis() + SONG_TONE_WAIT;
+      }
+    } else {
+      toneString[0] = 0;
+    }
+    updateLED();
+    redrawDisplay();
+  }
+}
+
+int abs(int x) {
+  return x < 0 ? -x : x;
+}
+
+int selectToneFromBatch() {
+  for (int i = 0; i < TONES_BATCH_SIZE; i++) {
+    int actCount = 0;
+    for (int j = 0; j < TONES_BATCH_SIZE; j++) {
+      if (abs(tonesBatch[i] - tonesBatch[j]) <= TONES_BATCH_TOLERANCE)
+        actCount++;
+    }
+    if (actCount >= TONES_BATCH_MINIMUM)
+      return tonesBatch[i];
+  }
+  return -1;
+}
+
+void updateLED() {
+  if (millis() >= nextLedChange) {
+    if (ledStatus == LOW) {
+      ledStatus = HIGH;
+      nextLedChange = millis() + LED_ON_TIME;
+    } else {
+      ledStatus = LOW;
+      nextLedChange = millis() + LED_OFF_TIME;
     }
   }
-  FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
-  FFT.Compute(vReal, vImg, SAMPLES, FFT_FORWARD);
-  FFT.ComplexToMagnitude(vReal, vImg, SAMPLES);
-  double freq = FFT.MajorPeak(vReal, SAMPLES, SAMPLING_FREQUENCY) / 2.0;
+}
 
-  int tone = freqToTone(freq);
-
-  toneToString(tone, toneString);
-  Serial.printf("%s (%lf Hz)\n", toneString, freq);
+void redrawDisplay() {
+  display.clear();
+  display.drawString(50, 20, toneString);
+  display.drawProgressBar(0, 50, 125, 10, relativeProgress);
+  display.display();
 }
--- src/tones.h
+++ src/tones.h
@@ -1,3 +1,136 @@
+#define C0 0
+#define C0x 1
+#define D0 2
+#define D0x 3
+#define E0 4
+#define F0 5
+#define F0x 6
+#define G0 7
+#define G0x 8
+#define A0 9
+#define A0x 10
+#define B0 11
+
+#define C1 12
+#define C1x 13
+#define D1 14
+#define D1x 15
+#define E1 16
+#define F1 17
+#define F1x 18
+#define G1 19
+#define G1x 20
+#define A1 21
+#define A1x 22
+#define B1 23
+
+#define C2 24
+#define C2x 25
+#define D2 26
+#define D2x 27
+#define E2 28
+#define F2 29
+#define F2x 30
+#define G2 31
+#define G2x 32
+#define A2 33
+#define A2x 34
+#define B2 35
+
+#define C3 36
+#define C3x 37
+#define D3 38
+#define D3x 39
+#define E3 40
+#define F3 41
+#define F3x 42
+#define G3 43
+#define G3x 44
+#define A3 45
+#define A3x 46
+#define B3 47
+
+#define C4 48
+#define C4x 49
+#define D4 50
+#define D4x 51
+#define E4 52
+#define F4 53
+#define F4x 54
+#define G4 55
+#define G4x 56
+#define A4 57
+#define A4x 58
+#define B4 59
+
+#define C5 60
+#define C5x 61
+#define D5 62
+#define D5x 63
+#define E5 64
+#define F5 65
+#define F5x 66
+#define G5 67
+#define G5x 68
+#define A5 69
+#define A5x 70
+#define B5 71
+
+#define C6 72
+#define C6x 73
+#define D6 74
+#define D6x 75
+#define E6 76
+#define F6 77
+#define F6x 78
+#define G6 79
+#define G6x 80
+#define A6 81
+#define A6x 82
+#define B6 83
+
+#define C7 84
+#define C7x 85
+#define D7 86
+#define D7x 87
+#define E7 88
+#define F7 89
+#define F7x 90
+#define G7 91
+#define G7x 92
+#define A7 93
+#define A7x 94
+#define B7 95
+
+#define C8 96
+#define C8x 97
+#define D8 98
+#define D8x 99
+#define E8 100
+#define F8 101
+#define F8x 102
+#define G8 103
+#define G8x 104
+#define A8 105
+#define A8x 106
+#define B8 107
+
+#define C9 108
+#define C9x 109
+#define D9 110
+#define D9x 111
+#define E9 112
+#define F9 113
+#define F9x 114
+#define G9 115
+#define G9x 116
+#define A9 117
+#define A9x 118
+#define B9 119
+
+
+
+
 
 double toneToFreq(int tone);
 int freqToTone(double freq);
